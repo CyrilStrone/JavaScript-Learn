b7ca7536a05d24cbd06b5c2878e9be6750bd52b3
Cyril_Strone <kidvos@gmail.com>
2022-03-06 03:34:15 +0700

    Основы 2

---
+++ Основы JavaScript/2.17 Функции-стрелки, основы/app.js
@@ -0,0 +1,15 @@
+alert("Hello");
+
+let age = prompt('Сколько тебе лет?', 100);
+
+alert(`Тебе ${age} лет!`)
+
+result = confirm(question);
+
+let isBoss = confirm("Ты здесь главный?");
+
+alert(isBoss); // true, если нажата OK
+
+
+let name = prompt('Ваше имя?', 100);
+alert(name)
