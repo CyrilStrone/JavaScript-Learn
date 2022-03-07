4f1c72103eadafb45c7b77960512301a2ea3095b
Cyril_Strone <kidvos@gmail.com>
2022-03-08 05:59:17 +0700

    Основы 4

--- Основы JavaScript/2.13 Циклы while и for/app.js
+++ Основы JavaScript/2.13 Циклы while и for/app.js
@@ -1,15 +1,120 @@
-alert("Hello");
 
-let age = prompt('Сколько тебе лет?', 100);
+let i = 0;
+while (i < 3) { // выводит 0, затем 1, затем 2
+  alert( i );
+  i++;
+}
 
-alert(`Тебе ${age} лет!`)
+let i = 3;
+while (i) { // когда i будет равно 0, условие станет ложным, и цикл остановится
+  alert( i );
+  i--;
+}
 
-result = confirm(question);
+let i = 3;
+while (i) alert(i--);
 
-let isBoss = confirm("Ты здесь главный?");
+let i = 0;
+do {
+  alert( i );
+  i++;
+} while (i < 3);
 
-alert(isBoss); // true, если нажата OK
+for (let i = 0; i < 3; i++) { // выведет 0, затем 1, затем 2
+  alert(i);
+}
 
+for (let i = 0; i < 3; i++) {
+  alert(i); // 0, 1, 2
+}
+alert(i); // ошибка, нет такой переменной
 
-let name = prompt('Ваше имя?', 100);
-alert(name)
+let i = 0; // мы уже имеем объявленную i с присвоенным значением
+
+for (; i < 3; i++) { // нет необходимости в "начале"
+  alert( i ); // 0, 1, 2
+}
+let i = 0;
+
+for (; i < 3;) {
+  alert( i++ );
+}
+
+for (;;) {
+  // будет выполняться вечно
+}
+
+let sum = 0;
+
+while (true) {
+
+  let value = +prompt("Введите число", '');
+
+  if (!value) break; // (*)
+
+  sum += value;
+
+}
+alert( 'Сумма: ' + sum );
+
+for (let i = 0; i < 10; i++) {
+
+  // если true, пропустить оставшуюся часть тела цикла
+  if (i % 2 == 0) continue;
+
+  alert(i); // 1, затем 3, 5, 7, 9
+}
+
+for (let i = 0; i < 10; i++) {
+
+  if (i % 2) {
+    alert( i );
+  }
+
+}
+
+for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 3; j++) {
+
+    let input = prompt(`Значение на координатах (${i},${j})`, '');
+
+    // Что если мы захотим перейти к Готово (ниже) прямо отсюда?
+
+  }
+}
+
+alert('Готово!');
+
+outer: for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 3; j++) {
+
+    let input = prompt(`Значение на координатах (${i},${j})`, '');
+
+    // если пустая строка или Отмена, то выйти из обоих циклов
+    if (!input) break outer; // (*)
+
+    // сделать что-нибудь со значениями...
+  }
+}
+
+alert('Готово!');
+
+let num;
+
+do {
+  num = prompt("Введите число больше 100?", 0);
+} while (num <= 100 && num);
+
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // Для всех i...
+
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  alert( i ); // простое число
+}
--- Основы JavaScript/2.14 Конструкция switch/app.js
+++ Основы JavaScript/2.14 Конструкция switch/app.js
@@ -1,15 +1,147 @@
-alert("Hello");
+switch(x) {
+    case 'value1':  // if (x === 'value1')
+      ...
+      [break]
+  
+    case 'value2':  // if (x === 'value2')
+      ...
+      [break]
+  
+    default:
+      ...
+      [break]
+  }
 
-let age = prompt('Сколько тебе лет?', 100);
+let a = 2 + 2;
 
-alert(`Тебе ${age} лет!`)
+switch (a) {
+  case 3:
+    alert( 'Маловато' );
+    break;
+  case 4:
+    alert( 'В точку!' );
+    break;
+  case 5:
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( "Нет таких значений" );
+}
 
-result = confirm(question);
+let a = 2 + 2;
 
-let isBoss = confirm("Ты здесь главный?");
+switch (a) {
+  case 3:
+    alert( 'Маловато' );
+  case 4:
+    alert( 'В точку!' );
+  case 5:
+    alert( 'Перебор' );
+  default:
+    alert( "Нет таких значений" );
+}
 
-alert(isBoss); // true, если нажата OK
+let a = "1";
+let b = 0;
 
+switch (+a) {
+  case b + 1:
+    alert("Выполнится, т.к. значением +a будет 1, что в точности равно b+1");
+    break;
 
-let name = prompt('Ваше имя?', 100);
-alert(name)
+  default:
+    alert("Это не выполнится");
+}
+
+let a = 2 + 2;
+
+switch (a) {
+  case 4:
+    alert('Правильно!');
+    break;
+
+  case 3: // (*) группируем оба case
+  case 5:
+    alert('Неправильно!');
+    alert("Может вам посетить урок математики?");
+    break;
+
+  default:
+    alert('Результат выглядит странновато. Честно.');
+}
+
+let arg = prompt("Введите число?");
+switch (arg) {
+  case '0':
+  case '1':
+    alert( 'Один или ноль' );
+    break;
+
+  case '2':
+    alert( 'Два' );
+    break;
+
+  case 3:
+    alert( 'Никогда не выполнится!' );
+    break;
+  default:
+    alert( 'Неизвестное значение' );
+}
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+if(browser == 'Edge') {
+    alert("You've got the Edge!");
+  } else if (browser == 'Chrome'
+   || browser == 'Firefox'
+   || browser == 'Safari'
+   || browser == 'Opera') {
+    alert( 'Okay we support these browsers too' );
+  } else {
+    alert( 'We hope that this page looks ok!' );
+  }
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+if (number === 0) {
+  alert('Вы ввели число 0');
+}
+
+if (number === 1) {
+  alert('Вы ввели число 1');
+}
+
+if (number === 2 || number === 3) {
+  alert('Вы ввели число 2, а может и 3');
+}
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch (number) {
+  case 0:
+    alert('Вы ввели число 0');
+    break;
+
+  case 1:
+    alert('Вы ввели число 1');
+    break;
+
+  case 2:
+  case 3:
+    alert('Вы ввели число 2, а может и 3');
+    break;
+}
