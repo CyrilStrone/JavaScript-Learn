6e589bd809f20e08ca6c03f84a39a73259cc098f
Cyril_Strone <kidvos@gmail.com>
2022-03-10 04:17:08 +0700

    Основы 5

--- Основы JavaScript/2.15 Функции/app.js
+++ Основы JavaScript/2.15 Функции/app.js
@@ -1,15 +1,169 @@
-alert("Hello");
+function showMessage() {
+    alert( 'Всем привет!' );
+  }
+function имя(параметры) {
+  ...тело...
+}
 
-let age = prompt('Сколько тебе лет?', 100);
+function showMessage(){
+    alert('Всем привет! ');
+}
+showMessage();
 
-alert(`Тебе ${age} лет!`)
+function showMessage() {
+  let message = "Привет, я JavaScript!"; // локальная переменная
 
-result = confirm(question);
+  alert( message );
+}
 
-let isBoss = confirm("Ты здесь главный?");
+showMessage(); // Привет, я JavaScript!
 
-alert(isBoss); // true, если нажата OK
+alert( message ); // <-- будет ошибка, т.к. переменная видна только внутри функции
+
+let userName = 'Вася';
+
+function showMessage() {
+  let message = 'Привет, ' + userName;
+  alert(message);
+}
+
+showMessage(); // Привет, Вася
+
+let userName = 'Вася';
+
+function showMessage() {
+  userName = "Петя"; // (1) изменяем значение внешней переменной
+
+  let message = 'Привет, ' + userName;
+  alert(message);
+}
+
+alert( userName ); // Вася перед вызовом функции
+
+showMessage();
+
+alert( userName ); // Петя, значение внешней переменной было изменено функцией
+
+let userName = 'Вася';
+
+function showMessage() {
+  let userName = "Петя"; // объявляем локальную переменную
+
+  let message = 'Привет, ' + userName; // Петя
+  alert(message);
+}
+
+// функция создаст и будет использовать свою собственную локальную переменную userName
+showMessage();
+
+alert( userName ); // Вася, не изменилась, функция не трогала внешнюю переменную
+
+function showMessage(from, text) { // аргументы: from, text
+  alert(from + ': ' + text);
+}
+
+showMessage('Аня', 'Привет!'); // Аня: Привет! (*)
+showMessage('Аня', "Как дела?"); // Аня: Как дела? (**)
+
+function showMessage(from, text = anotherFunction()) {
+  // anotherFunction() выполнится только если не передан text
+  // результатом будет значение text
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+let result = sum(1, 2);
+alert( result ); // 3
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('А родители разрешили?');
+  }
+}
+
+let age = prompt('Сколько вам лет?', 18);
+
+if ( checkAge(age) ) {
+  alert( 'Доступ получен' );
+} else {
+  alert( 'Доступ закрыт' );
+}
+
+function showMovie(age) {
+  if ( !checkAge(age) ) {
+    return;
+  }
+
+  alert( "Вам показывается кино" ); // (*)
+  // ...
+}
+
+function doNothing() { /* пусто */ }
+
+alert( doNothing() === undefined ); // true
+function doNothing() {
+    return;
+  }
+  
+  alert( doNothing() === undefined ); // true
 
 
-let name = prompt('Ваше имя?', 100);
-alert(name)
+  showMessage(..)     // показывает сообщение
+  getAge(..)          // возвращает возраст (в каком-либо значении)
+  calcSum(..)         // вычисляет сумму и возвращает результат
+  createForm(..)      // создаёт форму (и обычно возвращает её)
+  checkPermission(..) // проверяет доступ, возвращая true/false
+
+  function showPrimes(n) {
+  nextPrime: for (let i = 2; i < n; i++) {
+
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    alert( i ); // простое
+  }
+}
+
+function showPrimes(n) {
+
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
+
+    alert(i);  // простое
+  }
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if ( n % i == 0) return false;
+  }
+  return true;
+}
+
+
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Родители разрешили?');
+  }
+
+function checkAge(age) {
+  return (age > 18) || confirm('Родители разрешили?');
+}
+
+function getMin(num1,num2){
+    return (num1 < num2) ? num1:num2;
+}
+
+function pow(x, n) {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
--- Основы JavaScript/2.16 Function Expression/app.js
+++ Основы JavaScript/2.16 Function Expression/app.js
@@ -1,15 +1,154 @@
-alert("Hello");
+function sayHi() {
+  alert( "Привет" );
+}
+let sayHi = function() {
+  alert( "Привет" );
+};
 
-let age = prompt('Сколько тебе лет?', 100);
+function sayHi() {
+  alert( "Привет" );
+}
 
-alert(`Тебе ${age} лет!`)
+alert( sayHi ); // выведет код функции
 
-result = confirm(question);
+function sayHi() {   // (1) создаём
+  alert( "Привет" );
+}
 
-let isBoss = confirm("Ты здесь главный?");
+let func = sayHi;    // (2) копируем
 
-alert(isBoss); // true, если нажата OK
+func(); // Привет    // (3) вызываем копию (работает)!
+sayHi(); // Привет   //     прежняя тоже работает (почему бы нет)
+
+function sayHi() {
+  // ...
+}
+
+let sayHi = function() {
+  // ...
+};
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+function showOk() {
+  alert( "Вы согласны." );
+}
+
+function showCancel() {
+  alert( "Вы отменили выполнение." );
+}
+
+// использование: функции showOk, showCancel передаются в качестве аргументов ask
+ask("Вы согласны?", showOk, showCancel);
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+
+// Function Declaration
+function sum(a, b) {
+  return a + b;
+}
+
+// Function Expression
+let sum = function(a, b) {
+  return a + b;
+};
+
+sayHi("Вася"); // Привет, Вася
+
+function sayHi(name) {
+  alert( `Привет, ${name}` );
+}
+sayHi("Вася"); // ошибка!
+
+let sayHi = function(name) {  // (*) магии больше нет
+  alert( `Привет, ${name}` );
+};
+
+let age = prompt("Сколько Вам лет?", 18);
+
+// в зависимости от условия объявляем функцию
+if (age < 18) {
+
+  function welcome() {
+    alert("Привет!");
+  }
+
+} else {
+
+  function welcome() {
+    alert("Здравствуйте!");
+  }
+
+}
+
+// ...не работает
+welcome(); // Error: welcome is not defined
 
 
-let name = prompt('Ваше имя?', 100);
-alert(name)
+let age = 16; // присвоим для примера 16
+
+if (age < 18) {
+  welcome();               // \   (выполнится)
+                           //  |
+  function welcome() {     //  |
+    alert("Привет!");      //  |  Function Declaration доступно
+  }                        //  |  во всём блоке кода, в котором объявлено
+                           //  |
+  welcome();               // /   (выполнится)
+
+} else {
+
+  function welcome() {
+    alert("Здравствуйте!");
+  }
+}
+
+// здесь фигурная скобка закрывается,
+// поэтому Function Declaration, созданные внутри блока кода выше -- недоступны отсюда.
+
+welcome(); // Ошибка: welcome is not defined
+
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Привет!");
+  };
+
+} else {
+
+  welcome = function() {
+    alert("Здравствуйте!");
+  };
+
+}
+
+welcome(); // теперь всё в порядке
+
+
+
+
+
+
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome = (age < 18) ?
+  function() { alert("Привет!"); } :
+  function() { alert("Здравствуйте!"); };
+
+welcome(); // теперь всё в порядке
